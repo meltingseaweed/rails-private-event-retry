9ad2a6ef8e07e6470e7adbdd83c2f205e1080737
meltingseaweed <conor.seo@gmail.com>
2026-05-19 15:40:44 +0900

    Feat: Finish Sections "Setup" and "Events and users" No decorations yet

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,26 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @events = Event.new
+  end
+
+  def create
+    @events = current_user.created_events.build(event_params)
+    if @events.save
+      redirect_to root_path, notice: "Event created successfully :)"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+  def event_params
+    params.expect(event: [ :event_date, :location ])
+  end
+end
---
+++ db/migrate/20260519053539_add_user_to_events.rb
@@ -0,0 +1,5 @@
+class AddUserToEvents < ActiveRecord::Migration[8.1]
+  def change
+    add_reference :events, :creator, null: false, foreign_key: { to_table: :users }
+  end
+end
