bb2d5f5e4b9428e3c50c438052e1cdd6102ae848
meltingseaweed <conor.seo@gmail.com>
2026-05-19 16:11:35 +0900

    Style: Add source (just in case) remove uneeded model for views

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
+
+  has_many :attendings, foreign_key: :attended_event_id
+  has_many :attendees, through: :attendings, source: :attendee
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :events, foreign_key: "creator_id", class_name: "Event" # events?
+
+  has_many :attendings, foreign_key: :attendee_id
+  has_many :attended_events, through: :attendings, source: :attend_event
 end
